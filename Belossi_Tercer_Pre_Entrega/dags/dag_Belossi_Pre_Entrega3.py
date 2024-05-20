d2783e267fe32c8416b100cf178ddf12863a0323
mateobelossi <163935274+mateobelossi@users.noreply.github.com>
2024-05-20 20:15:17 -0300

    Create dag_Belossi_Pre_Entrega3.py

---
+++ Belossi_Tercer_Pre_Entrega/dags/dag_Belossi_Pre_Entrega3.py
@@ -0,0 +1,47 @@
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.bash import BashOperator
+from airflow.models import Variable
+from datetime import datetime
+
+PASSWORD_REDSHIFT = Variable.get("PASSWORD_REDSHIFT")
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 5, 20),
+    'retries': 1,
+}
+
+with DAG(
+    dag_id = 'Belossi_Pre_Entrega3',
+    description='ETL: Get Binance Coins values, clean & transform, and finally load to Redshift.',
+    default_args=default_args,
+    max_active_runs=1,
+    schedule_interval= "15 00 * * *",
+    tags=["binance_coins"],
+    catchup=False,
+) as dag:
+    start = DummyOperator(
+            task_id=f"start",
+            )
+
+    end = DummyOperator(
+            task_id=f"end",
+            )
+    
+    task1 = BashOperator(
+        task_id='get_binance_coins_values',
+        bash_command=f'''\
+            export DBNAME_REDSHIFT="data-engineer-database" && \
+            export SCHEMA_NAME_REDSHIFT="mateobelossi_coderhouse" && \
+            export USER_REDSHIFT="mateobelossi_coderhouse" && \
+            export PASS_REDSHIFT="{PASSWORD_REDSHIFT}" && \
+            export HOST_REDSHIFT="data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com" && \
+            export PORT_REDSHIFT="5439" && \
+            export TABLE_NAME_REDSHIFT="binance_coins" && \
+            python /opt/airflow/python_scripts/script_Belossi_Pre_Entrega3.py
+        '''
+    )
+
+    start >> task1 >> end
