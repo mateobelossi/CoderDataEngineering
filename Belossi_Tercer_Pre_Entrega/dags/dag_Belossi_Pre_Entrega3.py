9ea7e6f3e7908d58a5d91deb803d677db380bdbe
mateobelossi <163935274+mateobelossi@users.noreply.github.com>
2024-05-20 23:29:49 -0300

    Update dag_Belossi_Pre_Entrega3.py

--- Belossi_Tercer_Pre_Entrega/dags/dag_Belossi_Pre_Entrega3.py
+++ Belossi_Tercer_Pre_Entrega/dags/dag_Belossi_Pre_Entrega3.py
@@ -4,7 +4,7 @@ from airflow.operators.bash import BashOperator
 from airflow.models import Variable
 from datetime import datetime
 
-PASSWORD_REDSHIFT = Variable.get("PASSWORD_REDSHIFT")
+#PASSWORD_REDSHIFT = Variable.get("PASSWORD_REDSHIFT")
 
 default_args = {
     'owner': 'airflow',
@@ -32,16 +32,18 @@ with DAG(
     
     task1 = BashOperator(
         task_id='get_binance_coins_values',
-        bash_command=f'''\
+        bash_command='''\
             export DBNAME_REDSHIFT="data-engineer-database" && \
             export SCHEMA_NAME_REDSHIFT="mateobelossi_coderhouse" && \
             export USER_REDSHIFT="mateobelossi_coderhouse" && \
-            export PASS_REDSHIFT="{PASSWORD_REDSHIFT}" && \
+            export PASS_REDSHIFT="{{ var.value.PASSWORD_REDSHIFT }}" && \
             export HOST_REDSHIFT="data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com" && \
             export PORT_REDSHIFT="5439" && \
             export TABLE_NAME_REDSHIFT="binance_coins" && \
+            export DS_DATE="{{ ds }}" && \
+            export DS_TOMORROW="{{ macros.ds_add(ds, 1) }}" && \
             python /opt/airflow/python_scripts/script_Belossi_Pre_Entrega3.py
-        '''
+        ''',
     )
 
     start >> task1 >> end
